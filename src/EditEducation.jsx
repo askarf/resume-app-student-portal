2bce82cdcb0c1f36789e8e3e20e3bef0f1e65054
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-12-18 15:16:36 -0500

    created edit education

--- src/EditEducation.jsx
+++ src/EditEducation.jsx
@@ -1,7 +1,26 @@
-export function EditEducation() {
+export function EditEducation(props) {
   return (
     <div>
       <h1>Edit education</h1>
+      <div>
+        {props.students[0].education.map((education) => (
+          <form key={education.id}>
+            <div>
+              University: <input defaultValue={education.university} name="university" type="text" />
+            </div>
+            <div>
+              Degree: <input defaultValue={education.degree} name="degree" type="text" />
+            </div>
+            <div>
+              Start Date: <input defaultValue={education.start_date} name="start_date" type="text" />
+            </div>
+            <div>
+              End Date: <input defaultValue={education.end_date} name="end_date" type="text" />
+            </div>
+          </form>
+        ))}
+      </div>
+      <button>Update Button: </button>
     </div>
   );
 }
