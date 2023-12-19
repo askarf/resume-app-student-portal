75b2437a544a5a68f5f81973e861973daa8e5d61
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-12-19 11:43:26 -0500

    establish current user

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const jwt = localStorage.getItem("jwt");
 if (jwt) {
@@ -8,6 +8,7 @@ if (jwt) {
 
 export function Login() {
   const [errors, setErrors] = useState([]);
+  const [currentUserId, setCurrentUserId] = useState();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,17 +17,20 @@ export function Login() {
     axios
       .post("http://localhost:3000/sessions.json", params)
       .then((response) => {
-        console.log(response.data);
         axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
         localStorage.setItem("jwt", response.data.jwt);
+        setCurrentUserId(response.data.student_id);
         event.target.reset();
-        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
         console.log(error.response);
         setErrors(["Invalid email or password"]);
       });
   };
+
+  useEffect(() => {
+    console.log("currentUserId changed:", currentUserId);
+  }, [currentUserId]);
 
   return (
     <div id="login">
